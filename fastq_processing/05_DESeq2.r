1c15a5dbaa6293d9dd6238e6731deed313d0afd3
joelmarmir <marmirjoel@gmail.com>
2025-05-09 17:10:00 +0200

    add csv phenotable and deseq2 script template

---
+++ fastq_processing/05_DESeq2.r
@@ -0,0 +1,245 @@
+############################################
+##### Differential Expression Analysis #####
+############################################
+
+# Load libraries
+library(DESeq2)
+library(dplyr)
+library(readr)
+library(pheatmap)
+library(org.Mm.eg.db)
+library(AnnotationDbi)
+library(clusterProfiler)
+library(ggplot2)
+
+
+#--------------------------------------
+#------------ Functions ---------------
+#--------------------------------------
+
+############## Data preparation ##############
+
+# Define file paths
+phenodata_path <- "/users/genomics/jmartinez/data/05_tables/Aydin_2019/Aydin_2019_Ascl1_phenotab1.csv"
+counts_dir <- "/users/genomics/jmartinez/a_primary_cilia_project/05_counts/"
+
+# Read the phenotype table
+phenodata <- read.csv(phenodata_path, header = TRUE, stringsAsFactors = TRUE)
+
+# Ensure Replicate is a factor
+phenodata$Replicate <- factor(phenodata$Replicate)
+
+# Extract SRR codes (assuming they are in a specific column, adjust as needed)
+# Let's assume the column is named "SRR_code"
+srr_codes <- phenodata$SRA_id
+
+# Initialize an empty list to store data
+count_data_list <- list()
+
+# Iterate over SRR codes and find corresponding count files
+for (srr in srr_codes) {
+    print(paste("Processing SRR code:", srr))  # Troubleshooting print
+    
+    count_file <- list.files(counts_dir, pattern = paste0(srr, ".*txt$"), full.names = TRUE)
+    
+    if (length(count_file) == 1) {  # Ensure a single match
+        print(paste("Found count file:", count_file))  # Troubleshooting print
+        
+        count_table <- read.delim(count_file, header = TRUE, stringsAsFactors = FALSE, skip = 1)
+        
+        # Extract relevant columns: Geneid and counts (assumed to be column 7)
+        count_data <- count_table[, c(1, 7)]
+        colnames(count_data) <- c("Geneid", srr)
+        
+        # Store data
+        count_data_list[[srr]] <- count_data
+    } else {
+        print(paste("Count file not found or multiple files found for SRR code:", srr))  # Troubleshooting print
+    }
+}
+
+# Merge all count tables by Geneid
+counts <- Reduce(function(x, y) full_join(x, y, by = "Geneid"), count_data_list)
+
+# Set Geneid as row names
+rownames(counts) <- counts$Geneid
+counts <- counts[, -1]  # Remove Geneid column
+
+# Generate deseq object with counts and condition information
+dds <- DESeqDataSetFromMatrix(countData = counts,
+                              colData = phenodata,
+                              design = ~Replicate + Condition)
+
+############## Data visualization ##############
+
+vsd <- vst(dds, blind = TRUE)
+
+# Export PCA plot to PDF
+pdf("PCA_plot.pdf")
+plotPCA(vsd, intgroup = c("Condition", "Replicate"))
+dev.off()
+
+############## Deseq Analysis ##############
+
+# Filter genes with less than 10 reads total
+keep <- rowSums(counts(dds)) >= 10
+dds_modified <- dds[keep,]
+
+# Set uninduced as reference level
+dds_modified$Condition <- relevel(dds_modified$Condition, ref = "uninduced_0h")
+
+
+# Run DESeq and save results
+dds_modified <- DESeq(dds_modified)
+resultsNames(dds_modified)
+res_12h <- results(dds_modified, name = "Condition_induced_12h_vs_uninduced_0h")
+res_48h <- results(dds_modified, name = "Condition_induced_48h_vs_uninduced_0h")
+
+# Extract log2FoldChange and ENSEMBL identifier
+res_df_12h <- as.data.frame(res_12h)
+res_df_48h <- as.data.frame(res_48h)
+
+# Remove version number from gene names (to extract geneNames)
+res_df_12h$ENSEMBL <- rownames(res_df_12h)
+res_df_48h$ENSEMBL <- rownames(res_df_48h)
+res_df_12h$ENSEMBL_short <- gsub("\\..*", "",row.names(res_df_12h))
+res_df_48h$ENSEMBL_short <- gsub("\\..*", "",row.names(res_df_12h))
+
+# Map ENSEMBL IDs to gene symbols
+gene_symbols <- mapIds(org.Mm.eg.db, keys = res_df_12h$ENSEMBL_short, column = "SYMBOL", keytype = "ENSEMBL", multiVals = "first")
+
+# Add gene symbols to the results data frames
+res_df_12h$GeneSymbol <- gene_symbols
+res_df_48h$GeneSymbol <- gene_symbols
+
+# Reorder columns to place GeneSymbol first
+res_df_12h <- res_df_12h[, c("ENSEMBL", "GeneSymbol", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj", "baseMean")]
+res_df_48h <- res_df_48h[, c("ENSEMBL", "GeneSymbol", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj", "baseMean")]
+
+# Remove version number from gene symbols
+rownames(res_df_12h) <- NULL
+rownames(res_df_48h) <- NULL
+  
+write.csv(res_df_12h, file = "/users/genomics/jmartinez/data/06_log2fc/Aydin_2019/Aydin_2019_Ascl1_12h_DESeq2_results1.csv", row.names = FALSE)
+write.csv(res_df_48h, file = "/users/genomics/jmartinez/data/06_log2fc/Aydin_2019/Aydin_2019_Ascl1_48h_DESeq2_results1.csv", row.names = FALSE)
+
+#--------------------------------------
+#------- Results visualization --------
+#--------------------------------------
+
+############## heatmap ##############   
+
+# Select genes with padj > 0.05 and abs(logFC) > 2
+selected_genes_12h <- res_df_12h %>% filter(res_df_12h$padj < 0.05, abs(res_df_12h$log2FoldChange) > 2)
+selected_genes_48h <- res_df_48h %>% filter(res_df_48h$padj < 0.05, abs(res_df_48h$log2FoldChange) > 2)
+
+# Check the dimensions of the selected genes
+dim(selected_genes_12h)
+dim(selected_genes_48h)
+
+# Merge data frames by ENSEMBL ID
+selected_genes <- full_join(selected_genes_12h, selected_genes_48h, by = "ENSEMBL")
+
+# Remove duplicates
+selected_genes <- distinct(selected_genes)
+
+# Name rows with ENSMBL ID
+rownames(selected_genes) <- selected_genes$ENSEMBL
+selected_genes <- selected_genes[, -1]  # Remove ENSEMBL column
+
+rownames(selected_genes_12h) <- selected_genes_12h$ENSEMBL
+selected_genes_12h <- selected_genes_12h[, -1]  # Remove ENSEMBL column
+
+rownames(selected_genes_48h) <- selected_genes_48h$ENSEMBL
+selected_genes_48h <- selected_genes_48h[, -1]  # Remove ENSEMBL column
+
+# Check the dimensions of the merged data frame
+dim(selected_genes)
+
+# Visualize results in a heatmap
+# First normalize countsfrom dds object
+hmp_mat <- counts(dds_modified, normalized = TRUE)[(rownames(selected_genes_48h)),]
+
+# Get z-score and name samples
+hmp_zmat <- t(apply(hmp_mat, 1, scale))
+colnames(hmp_zmat) <- rownames(phenodata)
+
+# Export heatmap to PDF
+output_file_heatmap <- "heatmap.pdf"
+pdf(output_file_heatmap)
+pheatmap(hmp_zmat, cluster_cols = FALSE, cluster_rows = TRUE, annotation_col = phenodata[, c("Condition", "Replicate")])
+dev.off()
+
+
+############## Volcano plot ##############
+# Create a function to generate volcano plots with the names of the top 20 DEG
+create_volcano_plot <- function(res_df, title) {
+    res_df <- res_df %>% mutate(Significant = padj < 0.05 & abs(log2FoldChange) > 2)
+    
+    top_genes <- res_df %>% filter(Significant) %>% arrange(padj) %>% slice(1:20) %>% pull(GeneSymbol)
+    
+    ggplot(res_df, aes(x = log2FoldChange, y = -log10(padj), color = Significant)) +
+        geom_point(alpha = 0.5) +
+        scale_color_manual(values = c("grey", "red")) +
+        theme_minimal() +
+        labs(title = title, x = "Log2 Fold Change", y = "-Log10 Adjusted P-value") +
+        theme(legend.position = "none") +
+        geom_text(aes(label = ifelse(GeneSymbol %in% top_genes, GeneSymbol, "")), hjust = 1.5, vjust = 1.5)
+}
+
+# Generate volcano plots for 12h and 48h
+pdf("volcano_plot_12h.pdf")
+print(create_volcano_plot(res_df_12h, "Volcano Plot - 12h"))
+dev.off()
+
+pdf("volcano_plot_48h.pdf")
+print(create_volcano_plot(res_df_48h, "Volcano Plot - 48h"))
+dev.off()
+
+
+#-------------------------------------------
+# --------- Gene ontology -----------------
+# ------------------------------------------
+
+# Compute GO
+GO_results1 <- enrichGO(gene = rownames(selected_genes_12h),
+                       OrgDb = "org.Mm.eg.db",
+                       keyType = "ENSEMBL",
+                       ont = "BP")
+
+GO_results2 <- enrichGO(gene = rownames(selected_genes_48h),
+                        OrgDb = "org.Mm.eg.db",
+                        keyType = "ENSEMBL",
+                        ont = "BP")
+
+
+# Illustrate top 10 ontologies
+pdf("GO_results1_barplot.pdf")
+barplot(GO_results1, showCategory = 10)
+dev.off()
+
+pdf("GO_results1_dotplot.pdf")
+dotplot(GO_results1, showCategory = 10)
+dev.off()
+
+pdf("GO_results2_barplot.pdf")
+barplot(GO_results2, showCategory = 10)
+dev.off()
+
+pdf("GO_results2_dotplot.pdf")
+dotplot(GO_results2, showCategory = 10)
+dev.off()
+
+
+# Prova random
+matching_rows <- res_df_48h %>% filter(tolower(GeneSymbol) == "fbl")
+print(matching_rows)
+# Prova random
+matching_rows <- res_df_48h %>% filter(tolower(GeneSymbol) == "dkc1")
+print(matching_rows)
+# Prova random
+matching_rows <- res_df_48h %>% filter(tolower(GeneSymbol) == "trmt112")
+print(matching_rows)
+# Prova random
+matching_rows <- res_df_48h %>% filter(tolower(GeneSymbol) == "dimt1")
+print(matching_rows)
